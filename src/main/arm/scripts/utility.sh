0307d50e63130e04113b3fea5d98532121260e5b
Sanjay Mantoor <sanjay.mantoor@oracle.com>
2023-09-06 17:15:22 +0530

    Create utility.sh

---
+++ src/main/arm/scripts/utility.sh
@@ -0,0 +1,17 @@
+# Copyright (c) 2021, Oracle Corporation and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+# This script runs on Azure Container Instance with Alpine Linux that Azure Deployment script creates.
+
+function echo_stderr() {
+    echo >&2 "$@"
+    # The function is used for scripts running within Azure Deployment Script
+    # The value of AZ_SCRIPTS_OUTPUT_PATH is /mnt/azscripts/azscriptoutput
+    echo -e "$@" >>${AZ_SCRIPTS_PATH_OUTPUT_DIRECTORY}/errors.log
+}
+
+function echo_stdout() {
+    echo "$@"
+    # The function is used for scripts running within Azure Deployment Script
+    # The value of AZ_SCRIPTS_OUTPUT_PATH is /mnt/azscripts/azscriptoutput
+    echo -e "$@" >>${AZ_SCRIPTS_PATH_OUTPUT_DIRECTORY}/debug.log
+}
