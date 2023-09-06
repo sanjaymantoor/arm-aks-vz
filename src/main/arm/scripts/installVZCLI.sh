3737d5956353f875866f4b338ee079e7b17031fb
Sanjay Mantoor <sanjay.mantoor@oracle.com>
2023-09-06 13:05:12 +0530

    Update installVZCLI.sh

--- src/main/arm/scripts/installVZCLI.sh
+++ src/main/arm/scripts/installVZCLI.sh
@@ -1,20 +1,33 @@
-#
+echo "Script ${0} starts"
 
 #Function to display usage message
 function usage() {
-    usage=$(cat <<-END
-Usage:
-    ./installVZCLI.sh <vz cli url>
+    usage=$(
+        cat <<-END
+Specify the following ENV variables:
+VZ_CLI_DOWNLOAD
 END
+    )
+    echo_stdout ${usage}
+    if [ $1 -eq 0 ]; then
+        echo_stderr ${usage}
+        exit 1
+    fi
 }
-
 # Main script
 export script="${BASH_SOURCE[0]}"
 export scriptDir="$(cd "$(dirname "${script}")" && pwd)"
 
-read vzCliDownload
+VZ_CLI_DOWNLOAD=$1
 
 echo "Downloading vz cli"
-curl -LO $vzCliDownload
-curl -LO ${vzCliDownload}.sha256
-sha256sum -c verrazzano-1.6.5-linux-amd64.tar.gz.sha256
+curl -LO $VZ_CLI_DOWNLOAD
+curl -LO ${VZ_CLI_DOWNLOAD}.sha256
+fileName=`echo $VZ_CLI_DOWNLOAD | awk -F/ '{print $NF}'`
+sha256sum -c ${fileName}.sha256
+tar xvf ${fileName}
+dirName1=`echo $fileName | cut -f1 -d"-"`
+dirName2=`echo $fileName | cut -f2 -d"-"`
+dirName=${dirName1}-${dirName2}
+cp ${dirName}/bin/vz /usr/local/bin
+vz version
