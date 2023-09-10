280c3dccb753bef91ff18bf48f2a6b0b4c97a158
Sanjay Mantoor <sanjay.mantoor@oracle.com>
2023-09-10 17:15:11 +0530

    Update installVZ.sh

--- src/main/arm/scripts/installVZ.sh
+++ src/main/arm/scripts/installVZ.sh
@@ -1,95 +1,78 @@
-echo "Script ${0} starts"
+	echo "Script ${0} starts"
 
-#Function to display usage message
-function usage() {
-    usage=$(
-        cat <<-END
-Specify the following ENV variables:
-VZ_CLI_DOWNLOAD
-AKS_CLUSTER_RESOURCEGROUP_NAME
-AKS_CLUSTER_NAME
-END
-    )
-    echo_stdout ${usage}
-    if [ $1 -eq 2 ]; then
-        echo_stderr ${usage}
-        exit 1
-    fi
-}
+	#Function to display usage message
+	function usage() {
+		usage=$(
+			cat <<-END
+	Specify the following ENV variables:
+	VZ_CLI_DOWNLOAD
+	AKS_CLUSTER_RESOURCEGROUP_NAME
+	AKS_CLUSTER_NAME
+	CRD_FILE_DATA
+	END
+		)
+		echo_stdout ${usage}
+		if [ $1 -lt 4 ]; then
+			echo_stderr ${usage}
+			exit 1
+		fi
+	}
 
-#Function to validate input
-function validate_input() {
-    if [ -z "$VZ_CLI_DOWNLOAD" ]; then
-        echo_stderr "USER_PROVIDED_IMAGE_PATH is required. "
-        usage 1
-    fi
-    if [ -z "$AKS_CLUSTER_RESOURCEGROUP_NAME" ]; then
-        echo_stderr "AKS_CLUSTER_RESOURCEGROUP_NAME is required. "
-        usage 1
-    fi
-    if [ -z "$AKS_CLUSTER_NAME" ]; then
-        echo_stderr "AKS_CLUSTER_NAME is required. "
-        usage 1
-    fi
-    
-}
+	#Function to validate input
+	function validate_input() {
+		if [ -z "$VZ_CLI_DOWNLOAD" ]; then
+			echo_stderr "USER_PROVIDED_IMAGE_PATH is required. "
+			usage 1
+		fi
+		if [ -z "$AKS_CLUSTER_RESOURCEGROUP_NAME" ]; then
+			echo_stderr "AKS_CLUSTER_RESOURCEGROUP_NAME is required. "
+			usage 1
+		fi
+		if [ -z "$AKS_CLUSTER_NAME" ]; then
+			echo_stderr "AKS_CLUSTER_NAME is required. "
+			usage 1
+		fi
+		if [ -z "$CRD_FILE_DATA" ]; then
+			echo_stderr "Upload the vz crd data file"
+			usage 1
+		fi
+	   
+	}
 
-# Connect to AKS cluster
-function connect_aks_cluster() {
-    echo_stdout "Connecting to AKS cluster ${AKS_CLUSTER_NAME} for the resource group ${AKS_CLUSTER_RESOURCEGROUP_NAME}"
-    state=$(az aks get-credentials --resource-group ${AKS_CLUSTER_RESOURCEGROUP_NAME} --name ${AKS_CLUSTER_NAME} --overwrite-existing)
-    echo_stdout ${state}
-}
-# Main script
-export script="${BASH_SOURCE[0]}"
-export scriptDir="$(cd "$(dirname "${script}")" && pwd)"
-source ${scriptDir}/utility.sh
-./installVZCLI.sh ${VZ_CLI_DOWNLOAD}
+	# Connect to AKS cluster
+	function connect_aks_cluster() {
+		echo_stdout "Connecting to AKS cluster ${AKS_CLUSTER_NAME} for the resource group ${AKS_CLUSTER_RESOURCEGROUP_NAME}"
+		state=$(az aks get-credentials --resource-group ${AKS_CLUSTER_RESOURCEGROUP_NAME} --name ${AKS_CLUSTER_NAME} --overwrite-existing)
+		echo_stdout ${state}
+	}
+	# Main script
+	export script="${BASH_SOURCE[0]}"
+	export scriptDir="$(cd "$(dirname "${script}")" && pwd)"
+	source ${scriptDir}/utility.sh
+	./installVZCLI.sh ${VZ_CLI_DOWNLOAD}
 
-connect_aks_cluster
-export KUBECONFIG=$HOME/.kube/config
-echo_stdout "KUBECONFIG is set to $KUBECONFIG"
-echo_stdout "Installing vz using vz cli"
-vz install -f - <<EOF
-apiVersion: install.verrazzano.io/v1beta1
-kind: Verrazzano
-metadata:
-  name: example-verrazzano
-spec:
-  profile: prod
-  defaultVolumeSource:
-    persistentVolumeClaim:
-      claimName: globalOverride
-  volumeClaimSpecTemplates:
-    - metadata:
-        name: globalOverride
-      spec:
-        resources:
-          requests:
-            storage: 50Gi
-  components:
-    ingressNGINX:
-      overrides:
-      - values:
-          controller:
-            service:
-              annotations:
-                service.beta.kubernetes.io/azure-load-balancer-health-probe-request-path: /healthz
-EOF
-echo_stdout "Getting vz status"
-vz status
-vz status | grep 'State: Ready'
-if [[ $? != 0 ]]; then
-    echo_stderr "VZ installation is not successful"
-else
-    echo_stdout "VZ installation is successful"
-fi 
+	connect_aks_cluster
+	export KUBECONFIG=$HOME/.kube/config
+	echo_stdout "KUBECONFIG is set to $KUBECONFIG"
+	echo_stdout "Installing vz using vz cli"
+	echo "CRD File supplied"
+	echo ${CRD_FILE_DATA}
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-echo "VZ login details"
-echo "Username: verrazzano"
-echo "Password:"
-kubectl get secret \
-    --namespace verrazzano-system verrazzano \
-    -o jsonpath={.data.password} | base64 \
-    --decode; echo
+	vz install -f - <<EOF 
+	${CRD_FILE_DATA}
+	EOF
+	echo_stdout "Getting vz status"
+	vz status
+	vz status | grep 'State: Ready'
+	if [[ $? != 0 ]]; then
+		echo_stderr "VZ installation is not successful"
+	else
+		echo_stdout "VZ installation is successful"
+	fi 
+
+	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+	./kubectl version
+	echo "VZ login details"
+	echo "Username: verrazzano"
+	echo "Password:"
+	./kubectl get secret --namespace verrazzano-system verrazzano -o jsonpath={.data.password} | base64 -d; echo
