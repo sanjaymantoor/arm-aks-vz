15412af1f6e1a3c83c836d9e208ed5c4402394b0
Sanjay Mantoor <sanjay.mantoor@oracle.com>
2023-10-04 15:49:28 +0530

    Error handling
    Signed-off-by: Sanjay Mantoor <sanjay.mantoor@oracle.com>

--- src/main/arm/scripts/installVZ.sh
+++ src/main/arm/scripts/installVZ.sh
@@ -39,52 +39,21 @@
 	   
 	}
 
-	# Connect to AKS cluster
-	function connect_aks_cluster() {
-		echo_stdout "Connecting to AKS cluster ${AKS_CLUSTER_NAME} for the resource group ${AKS_CLUSTER_RESOURCEGROUP_NAME}"
-		state=$(az aks get-credentials --resource-group ${AKS_CLUSTER_RESOURCEGROUP_NAME} --name ${AKS_CLUSTER_NAME} --overwrite-existing)
-		echo_stdout ${state}
-	}
 	# Main script
 	export script="${BASH_SOURCE[0]}"
 	export scriptDir="$(cd "$(dirname "${script}")" && pwd)"
 	source ${scriptDir}/utility.sh
+	installUtilities
 	./installVZCLI.sh ${VZ_CLI_DOWNLOAD}
 	connect_aks_cluster
 	export KUBECONFIG=$HOME/.kube/config
 	echo_stdout "KUBECONFIG is set to $KUBECONFIG"
 	echo_stdout "Installing vz using vz cli"
-	echo "CRD File downloading from ${CRD_FILE_DATA}"
-	wget $VZ_CRD_FILE_URL
+	getVerrazzanoCRDFile $VZ_CRD_FILE_URL
 	fileName=`echo $VZ_CRD_FILE_URL | awk -F/ '{print $NF}'`
 	vz install -f $fileName >> ${AZ_SCRIPTS_PATH_OUTPUT_DIRECTORY}/debug.log 2>&1
-	sleep 1m
-	echo_stdout "Getting vz status"
-	vz status >> ${AZ_SCRIPTS_PATH_OUTPUT_DIRECTORY}/debug.log
-	attempt=1
+	waitForReady
 	vz status | grep 'Available Components: 26/26'
-	while [[ $? != 0 ]]
-	do
-		echo "Waiting for verrazzanon installation to complete"
-		sleep 30s
-		if [[ $attempt -gt 10 ]]; then
-			break
-		fi 
-		attempt=`expr($attempt+1)`
-		vz status | grep 'Available Components: 26/26'
-	done
-	vz status | grep 'Available Components: 26/26'
-	if [[ $? != 0 ]]; then
-		echo_stderr "VZ installation is not successful"
-	else
-		echo_stdout "VZ installation is successful"
-	fi 
+	checkStatus $? "VZ installation is not successful"
+	echo_stdout "VZ installation is successful"
 	vzStatus_jsonout
-	curl -LOqf "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" | true
-	./kubectl version | tee -a ${AZ_SCRIPTS_PATH_OUTPUT_DIRECTORY}/debug.log
-	echo_stdout "VZ login details" 
-	echo_stdout "Username: verrazzano"
-	echo_stdout "Password:"
-	./kubectl get secret --namespace verrazzano-system verrazzano -o jsonpath={.data.password} | base64 -d | tee -a ${AZ_SCRIPTS_PATH_OUTPUT_DIRECTORY}/debug.log
-	#./kubectl get secret --namespace verrazzano-system verrazzano -o jsonpath={.data.password} | base64 -d  >> $AZ_SCRIPTS_OUTPUT_PATH
-	sleep 1m
--- src/main/arm/scripts/utility.sh
+++ src/main/arm/scripts/utility.sh
@@ -16,8 +16,19 @@ function echo_stdout() {
     echo -e "$@" >>${AZ_SCRIPTS_PATH_OUTPUT_DIRECTORY}/debug.log
 }
 
+function checkStatus() {
+	statusCode=$1
+	statusMessage=$2
+	if [[ $statusCode != 0 ]]; then
+		echo_stderr $2
+		exit $statusCode
+	fi
+}
+
+# Write vz status output as JSON format so that deployment script can read it
 function vzStatus_jsonout() {
     vz status > vzStatusOut
+    checkStatus $? "Unable to get Verrazzano status"
     consoleUrl=`cat vzStatusOut | grep consoleUrl| awk '{print ""$2""}'`
     grafanaUrl=`cat vzStatusOut | grep grafanaUrl| awk '{print ""$2""}'`
 	keyCloakUrl=`cat vzStatusOut | grep keyCloakUrl| awk '{print ""$2""}'`
@@ -27,8 +38,51 @@ function vzStatus_jsonout() {
 	prometheusUrl=`cat vzStatusOut | grep prometheusUrl| awk '{print ""$2""}'`
 	rancherUrl=`cat vzStatusOut | grep prometheusUrl| awk '{print ""$2""}'`
     accessEndPoints="{consoleUrl=$consoleUrl},   {grafanaUrl=$grafanaUrl},   {keyCloakUrl=$keyCloakUrl},   {kialiUrl=$kialiUrl},   {openSearchDashboardsUrl=$openSearchDashboardsUrl},   {openSearchUrl=$openSearchUrl},   {prometheusUrl=$prometheusUrl},   {rancherUrl=$rancherUrl}"
-   result=$(jq -n -c --arg accessEndPoints "$accessEndPoints" '{accessEndPoints: $accessEndPoints}')
-   echo_stdout $result
-   echo $result >$AZ_SCRIPTS_OUTPUT_PATH 
-   rm -f vzStatusOut
+    result=$(jq -n -c --arg accessEndPoints "$accessEndPoints" '{accessEndPoints: $accessEndPoints}')
+    checkStatus $? "Unable to JSON format Verrazzano accessEndpoints output"
+    echo_stdout $result
+    echo $result >$AZ_SCRIPTS_OUTPUT_PATH 
+    rm -f vzStatusOut
+}
+
+# Connect to AKS cluster
+function connect_aks_cluster() {
+   echo_stdout "Connecting to AKS cluster ${AKS_CLUSTER_NAME} for the resource group ${AKS_CLUSTER_RESOURCEGROUP_NAME}"
+   state=$(az aks get-credentials --resource-group ${AKS_CLUSTER_RESOURCEGROUP_NAME} --name ${AKS_CLUSTER_NAME} --overwrite-existing)
+   echo_stdout ${state}
+}
+
+# Download Verrazzano CRD file
+function getVerrazzanoCRDFile() {
+	crdFileURL=$1
+	echo_stdout "CRD File downloading from ${crdFileURL}"
+	wget --no-check-certificate -q $crdFileURL
+	checkStatus $? "Failed to download ${crdFileURL}"
+	fileName=`echo $VZ_CRD_FILE_URL | awk -F/ '{print $NF}'`
+	ls $fileName
+	checkStatus $? "$fileName is not available locally"
+}
+
+# Wait for verrazzano installation
+function waitForReady() {
+	vz status >> ${AZ_SCRIPTS_PATH_OUTPUT_DIRECTORY}/debug.log
+	attempt=1
+	vz status | grep 'Available Components: 26/26'
+	while [[ $? != 0 ]]
+	do
+		echo "Waiting for verrazzanon installation to complete"
+		sleep 30s
+		if [[ $attempt -gt 10 ]]; then
+			break
+		fi 
+		attempt=`expr($attempt+1)`
+		vz status | grep 'Available Components: 26/26'
+	done
+}
+
+# Download and install dependencies
+function installUtilities() {
+	curl -LOqf "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" | true
+	./kubectl version | tee -a ${AZ_SCRIPTS_PATH_OUTPUT_DIRECTORY}/debug.log
+	checkStatus $? "kubectl is not installed or not available"
 }
